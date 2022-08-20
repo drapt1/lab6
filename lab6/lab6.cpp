356d2768f7cbd840b21ca8a9c945358024b6781f
Костаревы <@->
2022-08-20 16:14:25 +0500

    Добавьте файлы проекта.

---
+++ lab6/lab6.cpp
@@ -0,0 +1,16 @@
+﻿#include <string>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите строку ";
+    string s;
+    getline(cin, s);
+    sort(s.begin(), s.end());
+    cout << s;
+    return 0;
+}
